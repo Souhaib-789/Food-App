799a12cd45fc670f411953577988a3482d560f51
Souhaib <souhaibsouhaib838@gmail.com>
2023-01-06 15:30:53 +0500

    Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,17 @@ import axios from 'axios';
 import SignIn from './screens/login/Login';
 import Signup from './screens/signup/Signup';
 import { Route, Routes } from 'react-router-dom';
+import Home from './screens/Home/Home';
 
 function App() {
 
-  useEffect(()=>{
-    axios.get('https://api.spoonacular.com/food/products/search?query=pizza&apiKey=c67f7a16ab07455db25586f302928b7d')
-    .then(res => {
-      console.log(res);
-    })
-    .catch(err => {
-      console.log(err);
-    })
-  },[])
+
   return (
     <div className="App">
       <Routes>
-        <Route path='/' element={<SignIn/>} />
+        {/* <Route path='/' element={<SignIn/>} /> */}
         <Route path='/signup' element={<Signup/>} />
+        <Route path='/' element={<Home/>} />
       </Routes>
     
     
